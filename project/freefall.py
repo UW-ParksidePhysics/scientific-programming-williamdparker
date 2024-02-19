229e5e6e4bae23dec607e609f79cbb3aa100169a
William Parker <parkerw@uwp.edu>
2024-02-19 15:22:55 +0000

    Set up on replit for project

---
+++ project/freefall.py
@@ -0,0 +1,15 @@
+# from astropy.constants import g0
+# import numpy as np
+
+
+def calculate_positions(times, initial_position, initial_velocity, acceleration):
+    positions = []
+    return positions
+
+
+
+if __name__ == '__main__':
+  # gravitational_acceleration = g0.value
+  gravitational_acceleration = 9.81  # m/s^2
+  print(f'g = {gravitational_acceleration} m/s/s')
+
